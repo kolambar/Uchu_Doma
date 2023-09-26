d2a460ca21e8c220ebe4c163a6c7efc35b276d45
Nikolai <kolamba.r@gmail.com>
2023-09-26 18:39:37 +0300

    views for Lesson and Course

---
+++ courses/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from rest_framework import routers
+
+from courses.views.course import CourseViewSet
+from courses.views.lesson import *
+
+urlpatterns = [
+    path('', LessonListView.as_view()),
+    path('create/', LessonCreateView.as_view()),
+    path('<int:pk>/', LessonDetailView.as_view()),
+    path('<int:pk>/update', LessonUpdateView.as_view()),
+    path('<int:pk>/delete', LessonDeleteView.as_view()),
+]
+
+router = routers.SimpleRouter()
+router.register('course', CourseViewSet)
+
+urlpatterns += router.urls
---
+++ courses/views/lesson.py
@@ -0,0 +1,29 @@
+from rest_framework.generics import RetrieveAPIView, DestroyAPIView, ListAPIView, UpdateAPIView, CreateAPIView
+
+from courses.models import Lesson
+from courses.serializers.lesson import LessonSerializer
+
+
+class LessonDetailView(RetrieveAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonListView(ListAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonCreateView(CreateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonUpdateView(UpdateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonDeleteView(DestroyAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
